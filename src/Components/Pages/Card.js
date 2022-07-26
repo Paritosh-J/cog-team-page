0eee545b86467cccdefad0ec698b2cf4e3ba2530
Paritosh-J <85030129+Paritosh-J@users.noreply.github.com>
2022-07-26 12:47:13 +0530

    Update Card.js

--- src/Components/Pages/Card.js
+++ src/Components/Pages/Card.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Card.css";
-import mail from "../Images/mail.png";
+import { SiGmail } from "react-icons/si"
+// import mail from "../Images/mail.png";
 import github from "../Images/github.png";
 import linkedin from "../Images/linkedin.png";
 import instagram from "../Images/instagram.png";
@@ -21,15 +22,14 @@ const Card = (prop) => {
               </span>
             </h2>
             <div class="data">
-              <p>
-                {prop.desc}
-              </p>
+              <p>{prop.desc}</p>
 
               <div class="Media_icons">
                 <ul>
                   <li class="icon Gmail">
-                    <a href={prop.mail} target="_blank">
-                      <img src={mail} alt="https://www.google.com" />
+                    <a href="mailto:{prop.mail}" target="_blank">
+                      {/* <img src={mail} alt="https://www.google.com" /> */}
+                      <SiGmail />
                     </a>
                   </li>
                   <li class="icon linkedin">
